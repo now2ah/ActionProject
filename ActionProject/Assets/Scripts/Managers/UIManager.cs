490fa4a2d0f6e82718cd407b5874a63a0967d283
now2ah <hhw3287@hanmail.net>
2023-09-04 00:51:05 +0900

    UI Manager 구현

---
+++ ActionProject/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Action.Util;
+
+namespace Action.Manager
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        GameObject mainCanvasObject;
+        Canvas mainCanvas;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            base.SetName("UIManager");
+
+            _CreateMainCanvas();
+        }
+
+        public GameObject CreateUI(string name)
+        {
+            GameObject obj = Instantiate(Resources.Load(name) as GameObject);
+
+            if (null == obj)
+                Debug.LogError("UI Prefab is missing.");
+
+            if (null != mainCanvas)
+                obj.transform.SetParent(mainCanvas.transform, false);
+
+            return obj;
+        }
+
+        void _CreateMainCanvas()
+        {
+            mainCanvasObject = Instantiate(Resources.Load("MainCanvas") as GameObject);
+            mainCanvasObject.transform.SetParent(this.transform, false);
+            mainCanvas = mainCanvasObject.GetComponentInChildren<Canvas>();
+        }
+    }
+}
