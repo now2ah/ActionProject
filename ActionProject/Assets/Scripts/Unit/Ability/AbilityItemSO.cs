3cd6c947b53304603c306d46e766bd63fa3ce804
now2ah <hhw3287@hanmail.net>
2024-07-24 18:09:12 +0900

    AbilityItem 작업 / Hunt Stage 씬 작업

---
+++ ActionProject/Assets/Scripts/Unit/Ability/AbilityItemSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Action.SO
+{
+    [CreateAssetMenu(fileName = "AbilityItem", menuName = "ScriptableObject/AbilityItemSO", order = 3)]
+    public class AbilityItemSO : ScriptableObject
+    {
+        public string abilityName;
+        public string abilityDescription;
+    }
+}
