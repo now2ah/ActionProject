383e495aec593301de9ee733bf9975d776c9a266
now2ah <hhw3287@hanmail.net>
2025-02-19 16:18:23 +0900

    InputManager ScriptableObject로 생성 / MoveAction 구현 / Test

--- ActionProject/Assets/Scripts/Managers/InputManagerSO.cs
+++ ActionProject/Assets/Scripts/Managers/InputManagerSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,49 @@ namespace Action.SO
     [CreateAssetMenu(fileName = "InputManager", menuName = "ScriptableObject/InputManagerSO")]
     public class InputManagerSO : ScriptableObject
     {
+        [SerializeField]
+        public InputActionAsset IAAsset;
+
+        public event EventHandler<Vector2> OnMoveActionDown;
+        public event EventHandler<Vector2> OnMoveAction;
+        public event EventHandler<Vector2> OnMoveActionUp;
+
         InputAction moveAction;
+
+        private void OnEnable()
+        {
+            moveAction = IAAsset.FindAction("Move");
+
+            //moveAction.started += OnMoveActionStarted;
+            moveAction.performed += OnMoveActionPerformed;
+            //moveAction.canceled += OnMoveActionCanceled;
+
+            moveAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            //moveAction.started -= OnMoveActionStarted;
+            moveAction.performed -= OnMoveActionPerformed;
+            //moveAction.canceled -= OnMoveActionCanceled;
+
+            moveAction.Disable();
+        }
+
+        void OnMoveActionStarted(InputAction.CallbackContext context)
+        {
+            OnMoveActionDown.Invoke(this, context.ReadValue<Vector2>());
+        }
+
+        void OnMoveActionPerformed(InputAction.CallbackContext context)
+        {
+            OnMoveAction.Invoke(this, context.ReadValue<Vector2>());
+        }
+
+        void OnMoveActionCanceled(InputAction.CallbackContext context)
+        {
+            OnMoveActionUp.Invoke(this, context.ReadValue<Vector2>());
+        }
     }
 }
 
--- ActionProject/Assets/Scripts/Test/CinemachineTest.cs
+++ ActionProject/Assets/Scripts/Test/CinemachineTest.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using Action.CameraSystem;
+using Action.SO;
 
 public class CinemachineTest : MonoBehaviour
 {
     public CinemachineVirtualCamera vCam1;
     public CinemachineVirtualCamera vCam2;
+    public InputManagerSO inputManager;
 
     CinemachineBrain brain;
+
+    void _OnMoveAction(object o, Vector2 value)
+    {
+        Debug.Log(value);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         brain = Camera.main.GetComponent<CinemachineBrain>();
+        inputManager.OnMoveAction += _OnMoveAction;
     }
 
     // Update is called once per frame
